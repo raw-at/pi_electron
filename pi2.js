fa1a71346e84d8d5df162760822fe14105d3c65e
neeraj <neerajk207@gmail.com>
2017-04-04 12:13:19 +0530

    wifi connect done

--- pi2.js
+++ pi2.js
@@ -1,3 +1,8 @@
+var Dialogs = require('dialogs');
+var WiFiControl = require('wifi-control');
+var dialogs = Dialogs();
+
+
 function openCity(evt, cityName) {
     // Declare all variables
     var i, tabcontent, tablinks;
@@ -24,30 +29,83 @@ function wifi_access(){
 
     
 
-    var WiFiControl = require('wifi-control');
+    
  
   //  Initialize wifi-control package with verbose output 
-  WiFiControl.init({
-    debug: true
-  });
+    WiFiControl.init({
+        debug: true
+    });
  
   //  Try scanning for access points: 
-  WiFiControl.scanForWiFi( function(err, response) {
-    if (err) console.log(err);
+    WiFiControl.scanForWiFi( function(err, response) {
+        if (err) console.log(err);
 
-    // document.write(JSON.stringify(response.networks[0].ssid));
-     var list = document.getElementById("demo");
+        // document.write(JSON.stringify(response.networks[0].ssid));
+        var list = document.getElementById("demo");
+        while (list.firstChild) {
+            list.removeChild(list.firstChild);
+        }
+        for( var i=0; i < response.networks.length; i++){
+
+            var element = document.createElement("button");
+            var button_name = response.networks[i].ssid;
+            element.appendChild(document.createTextNode(button_name));
+            // element.setAttribute("id",i);
+            element.setAttribute("value",button_name);
+
+            // calling dialog_box function for password entry
+            element.onclick = dialog_box;
+            list.appendChild(element);
+        } 
+
+
+        // document.onclick = function(event) {
+        // var el = event.target;
+        // // var wifi_name = document.getElementById(el.id).value;
+        // var wifi_name = el.value;
+        // var password = "";
+        // dialogs.prompt('Enter password: ',function(response){
+        //     password = response;
+        //     wifi_connect(wifi_name, password);
+        //     // console.log(password); 
+        // }); 
+        
+        // console.log(password);
+        
+        // };
+    });
+
+
+
+
+        // var entry = document.createElement('a');
+        // var item = document.createElement('li');
+        // var item_text = response.networks[i].ssid;
+        // entry.appendChild(item);
+        // item.appendChild(document.createTextNode(item_text));
+        // list.appendChild(item);
+
+    
+    // var elem = document.getElementsByName("button");
+    // for ( var j=0; j < elem.length;j++){
+    //     elem[j].onclick = function(){
+    //         prompt("Enter password");
+    //     }
+    // }
+   
+
+       
+
      
-    for( var i=0; i < response.networks.length; i++){
-        var entry = document.createElement('li');
-        var item = JSON.stringify(response.networks[i].ssid);
-        entry.appendChild(document.createTextNode(item));
-        list.appendChild(entry);
 
-    }
-});
+    // if (el.nodeName == "button") {
+    //     alert("div.new clicked");
+    // }
 
-}
+
+
+
+
 
 
 
@@ -62,4 +120,43 @@ function wifi_access(){
 //     }
 
 // }
+}
+
+
+function dialog_box(event){
+
+    // document.onclick = function(event) {
+        var el = event.target;
+        // var wifi_name = document.getElementById(el.id).value;
+        var wifi_name = el.value;
+        var password = "";
+        dialogs.prompt('Enter password: ',function(response){
+            password = response;
+            wifi_connect(wifi_name, password);
+            // console.log(password); 
+        }); 
+        
+        console.log(password);
+        
+        // };
+}
+
+function wifi_connect(wifi_name, password){
+
+
+    console.log(wifi_name + password);
+
+    var _ap = {
+    ssid: wifi_name,
+    password: password
+    };
+    var results = WiFiControl.connectToAP( _ap, function(err, response) {
+        if (err) console.log(err);
+        console.log(response);
+    });
+
+};
+
+
+
  
